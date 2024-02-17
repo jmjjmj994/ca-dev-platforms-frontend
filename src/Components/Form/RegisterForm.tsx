29ef61a51283b6a326d21626fb2b8aebe71c39d8
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-02-17 14:49:22 +0100

    Register function and components finished

--- src/Components/Form/RegisterForm.tsx
+++ src/Components/Form/RegisterForm.tsx
@@ -1,6 +1,75 @@
+import { useState } from 'react';
+import { createUser, UserCreationData } from '../../fetch/fetch';
+import { Navigate } from 'react-router-dom';
 const RegisterForm = () => {
+  const [user, setUser] = useState<UserCreationData>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUser = ({
+    id,
+    value,
+  }: {
+    id: keyof UserCreationData;
+    value: string;
+  }) => {
+    switch (id) {
+      case 'firstName':
+        setFirstName(value);
+        break;
+      case 'lastName':
+        setLastName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+    }
+
+    setUser((prevUser) => ({ ...prevUser, [id]: value }));
+  };
+
+  const validate = async () => {
+    let isValid = true;
+    if (!user.firstName || !user.lastName || !user.email || !user.password) {
+      isValid = false;
+      console.log(user);
+    }
+
+    console.log('valid', isValid);
+
+    if (isValid) {
+      await createUser({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password,
+      });
+      setFirstName('');
+      setLastName('');
+      setEmail('');
+      setPassword('');
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => e.preventDefault();
+
   return (
-    <form className="user-form flex flex-col items-center justify-center gap-5">
+    <form
+      onSubmit={handleSubmit}
+      className="user-form flex flex-col items-center justify-center gap-5"
+    >
       <div className="flex flex-col  w-full">
         <label htmlFor="first-name">First name</label>
         <input
@@ -8,15 +77,23 @@ const RegisterForm = () => {
           type="text"
           id="first-name"
           placeholder="First name"
+          value={firstName}
+          onChange={(event) =>
+            handleUser({ id: 'firstName', value: event.target.value })
+          }
         />
       </div>
       <div className="flex flex-col w-full">
-        <label htmlFor="last-name">First name</label>
+        <label htmlFor="last-name">Last name</label>
         <input
           className="bg-light-grey border border-dark-grey py-2 pl-2 rounded-md"
           type="text"
           id="last-name"
           placeholder="Last name"
+          value={lastName}
+          onChange={(event) =>
+            handleUser({ id: 'lastName', value: event.target.value })
+          }
         />
       </div>
 
@@ -27,19 +104,31 @@ const RegisterForm = () => {
           type="text"
           id="email"
           placeholder="Email "
+          value={email}
+          onChange={(event) =>
+            handleUser({ id: 'email', value: event.target.value })
+          }
         />
       </div>
       <div className="flex flex-col w-full">
         <label htmlFor="password">Password</label>
         <input
           className="bg-light-grey border border-dark-grey py-2 pl-2 rounded-md"
-          type="text"
+          type="password"
           id="password"
           placeholder="Password"
+          value={password}
+          onChange={(event) =>
+            handleUser({ id: 'password', value: event.target.value })
+          }
         />
       </div>
       <div className="flex flex-col w-full">
-        <button className="bg-primary border border-dark-grey py-3 pl-2 mt-5 rounded-md text-light-grey">
+        <button
+          onClick={async () => validate()}
+          type="submit"
+          className="bg-primary border border-dark-grey py-3 pl-2 mt-5 rounded-md text-light-grey"
+        >
           Register
         </button>
       </div>
--- src/fetch/fetch.ts
+++ src/fetch/fetch.ts
@@ -22,14 +22,65 @@ https://ca-dev-platforms.onrender.com/api/getuser
 
 */
 
-const createUser = () => {
-  const url:string = 'https://ca-dev-platforms.onrender.com/api/signup';
+import { json } from 'react-router-dom';
+
+export interface UserCreationData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+interface UserCreationResponse {
+  id: string;
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+}
+
+export const createUser = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}: UserCreationData) => {
+  const url: string = 'https://ca-dev-platforms.onrender.com/api/signup';
 
   try {
-   const response = await fetch(url, )
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      }),
+      headers: { 'Content-type': 'application/json' },
+    });
 
-  }catch(error) {
-
+    console.log(response, 'response');
+    const {
+      id,
+      access_token,
+      expires_in,
+      refresh_token,
+    }: UserCreationResponse = await response.json();
+    const data = {
+      id: id,
+      access_token: access_token,
+      expires_in: expires_in,
+      refresh: refresh_token,
+    };
+    console.log(data);
+    localStorage.setItem(
+      'user',
+      JSON.stringify({
+        access_token: data.access_token,
+        userId: data.id,
+        loggedIn: true,
+      })
+    );
+  } catch (error) {
+    console.error(error);
   }
-
 };
