718f7fd0a071783bca843336daca7387c41c6964
Jonathan Braaten Mjøs <111571683+jmjjmj994@users.noreply.github.com>
2024-02-17 11:48:02 +0100

    Creating fetch folder for handling fetching on the frontend

---
+++ src/fetch/fetch.ts
@@ -0,0 +1,35 @@
+/* GET
+https://ca-dev-platforms.onrender.com/api/cars
+
+POST
+
+https://ca-dev-platforms.onrender.com/api/cars
+
+PUT
+
+https://ca-dev-platforms.onrender.com/api/cars/id
+
+DELETE
+
+https://ca-dev-platforms.onrender.com/api/cars/id
+
+POST
+https://ca-dev-platforms.onrender.com/api/signup
+
+GET
+
+https://ca-dev-platforms.onrender.com/api/getuser
+
+*/
+
+const createUser = () => {
+  const url:string = 'https://ca-dev-platforms.onrender.com/api/signup';
+
+  try {
+   const response = await fetch(url, )
+
+  }catch(error) {
+
+  }
+
+};
